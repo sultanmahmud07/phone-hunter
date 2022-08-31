0a503ffb0acf8166542804ef2971d3a9f42d7769
sultanmahmud07 <asadkhan01862@gmail.com>
2022-09-01 01:01:51 +0600

    some code added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//     modiul number 34   //
+/****************************/
 const loadPhones = async(searchText) =>{
   const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(url);
@@ -7,12 +9,28 @@ const loadPhones = async(searchText) =>{
 
 //  ==========2nd function call===========
 const displayPhones = phones =>{
-  //********* */ phone show maximum 20 ********
-   phones = phones.slice(0, 20);
+  //* phone show maximum 20
+  const showAll =document.getElementById('show-all');
+  if(phones.length > 20){
+    phones = phones.slice(0, 20);
+    showAll.classList.remove('d-none')
 
-   
+  }
+  else{
+    showAll.classList.add('d-none')
+  }
+  // display no phone found 
+  const noPhones =document.getElementById('alart-section');
+  if(phones.length === 0){
+   noPhones.classList.remove('d-none')
+  }
+  else{
+   noPhones.classList.add('d-none')
+  };
+  
   const phonesContainer =document.getElementById('phones-container');
-phonesContainer.innerText ='';
+
+   phonesContainer.innerText ='';
   // ===========loop throe here =============
   phones.forEach(phone =>{
     const phoneDiv =document.createElement('div');
@@ -23,21 +41,36 @@ phonesContainer.innerText ='';
       <div class="card-body">
         <h5 class="card-title">${phone.phone_name}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <button class="btn btn-primary" type="button">Detales</button>
       </div>
     </div>
     `;
     phonesContainer.append(phoneDiv);
   })
-  
+  // stop spenner 
+  togglySpenner(false)
   console.log(phones);
 }
-
+// search button onclink function =================
 document.getElementById('button-addon2').addEventListener('click', function(){
+  // loader start 
+  togglySpenner(true);
   const inputField =document.getElementById('input-field');
   const searchText =inputField.value;
   loadPhones(searchText)
   console.log(searchText)
 })
+//  function for loader
+const togglySpenner = isLoader =>{
+  const loaderSection =document.getElementById('loader');
+  if(isLoader){
+    loaderSection.classList.remove('d-none')
+  }
+  else{
+    loaderSection.classList.add('d-none')
+  }
+}
+
 
 //============ 1st function call here ============
-loadPhones()
+// loadPhones()
